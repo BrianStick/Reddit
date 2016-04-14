68160390317edaad56c9de5a786c8a17f47722ba
Brian Stickney <brianchase055@gmail.com>
2016-04-14 16:35:00 -0500

    correct classes

---
+++ Reddit/Models/comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Reddit.Models
+{
+    public class comment
+    {
+        public virtual post  Parentpost {get;set;}
+
+        public string body { get; set; }
+        public string comments { get; set; }
+        public DateTime posted { get; set; }
+
+    }
+}
---
+++ Reddit/Models/homepage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Reddit.Models
+{
+    public class post
+    {
+
+        public string id { get; set; }
+        [Required]
+        public string author { get; set; }
+        public string comments { get; set; }
+        public int upvote { get; set; }
+        public int downvote { get; set; }
+        [Required]
+        public string body { get; set; }
+        public string link { get; set; }
+        public string comment { get; set; }
+        public DateTime posted { get; set; }
+
+    }
+    public virtual ICollection<Comments> Comments { get; set; } =  new List comments
+};
